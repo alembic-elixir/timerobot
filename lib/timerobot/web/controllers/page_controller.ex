6ee7f5d8e4e0da6580264250ad6a2831c65b8315
Peter-Fagan <peter_fagan@hotmail.com>
2017-04-23 16:53:46 +1000

    Create header

--- lib/timerobot/web/controllers/page_controller.ex
+++ lib/timerobot/web/controllers/page_controller.ex
@@ -1,7 +1,11 @@
 defmodule Timerobot.Web.PageController do
   use Timerobot.Web, :controller
 
+  alias Timerobot.Timesheet
+
   def index(conn, _params) do
-    render conn, "index.html"
+    render conn, "index.html",
+    page_name: "home"
   end
+
 end
