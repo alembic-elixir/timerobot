9f6175d5222fac2c90e2aca4455acaf0e9d4050a
Peter-Fagan <peter_fagan@hotmail.com>
2017-05-05 15:22:34 +1000

    Add drop down selections for person and project in new entry

--- lib/timerobot/web/controllers/entry_controller.ex
+++ lib/timerobot/web/controllers/entry_controller.ex
@@ -1,26 +1,52 @@
 defmodule Timerobot.Web.EntryController do
   use Timerobot.Web, :controller
 
+  import Ecto
+  import Ecto.Query
+
   alias Timerobot.Timesheet
+  alias Timerobot.Timesheet.Person
+  alias Timerobot.Timesheet.Project
+  alias Timerobot.Repo
 
   def index(conn, _params) do
     entry = Timesheet.list_entry()
     render(conn, "index.html", entry: entry)
   end
 
-  def new(conn, _params) do
+  def new(conn, params) do
+    people =
+      Person
+      |> select([p], {p.name, p.id})
+      |> Repo.all
+
+    projects =
+      Project
+      |> select([p], {p.name, p.id})
+      |> Repo.all
+
     changeset = Timesheet.change_entry(%Timerobot.Timesheet.Entry{})
-    render(conn, "new.html", changeset: changeset)
+    render(conn, "new.html", changeset: changeset, people: people, projects: projects)
   end
 
   def create(conn, %{"entry" => entry_params}) do
+    people =
+      Person
+      |> select([p], {p.name, p.id})
+      |> Repo.all
+
+    projects =
+      Project
+      |> select([p], {p.name, p.id})
+      |> Repo.all
+
     case Timesheet.create_entry(entry_params) do
       {:ok, entry} ->
         conn
         |> put_flash(:info, "Entry created successfully.")
         |> redirect(to: entry_path(conn, :show, entry))
       {:error, %Ecto.Changeset{} = changeset} ->
-        render(conn, "new.html", changeset: changeset)
+        render(conn, "new.html", changeset: changeset, people: people, projects: projects)
     end
   end
 
