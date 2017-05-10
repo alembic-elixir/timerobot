c63b331a2cf1224bab603897840dda0c46942665
Peter-Fagan <peter_fagan@hotmail.com>
2017-05-10 11:46:30 +1000

    Fix person schema

--- lib/timerobot/timesheet/person.ex
+++ lib/timerobot/timesheet/person.ex
@@ -2,12 +2,14 @@ defmodule Timerobot.Timesheet.Person do
   use Ecto.Schema
 
   alias Timerobot.Timesheet.Entry
+  alias Timerobot.Timesheet.Project
 
   schema "timesheet_person" do
     field :name, :string
     field :slug, :string
 
-    has_many :project, Entry
+    has_many :projects, Project
+    has_many :entries, Entry
 
     timestamps()
   end
