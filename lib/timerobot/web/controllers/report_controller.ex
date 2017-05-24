1404138a85e02781a4cd5c1bc8badc8485f469db
Josh Price <josh@alembic.com.au>
2017-05-24 17:28:28 +1000

    Fix warnings

--- lib/timerobot/web/controllers/report_controller.ex
+++ lib/timerobot/web/controllers/report_controller.ex
@@ -3,18 +3,16 @@ defmodule Timerobot.Web.ReportController do
 
   alias Timerobot.Timesheet
 
-  def report(conn, report_params = %{"report" => %{"date" => date_string}}) do
+  def report(conn, %{"report" => %{"date" => date_string}}) do
     do_report conn, Timex.parse!(date_string, "%Y-%m-%d", :strftime)
   end
 
-  def report(conn, params) do
+  def report(conn, _params) do
     do_report conn, Timex.now
   end
 
   defp do_report(conn, date) do
-    {beginning_of_week, end_of_week} =
-      Timesheet.week_for_date(date)
-
+    {beginning_of_week, _} = Timesheet.week_for_date(date)
     data =
       date
       |> Timesheet.entries_for_week
