91602c530d1b530796bd4c52dd0c20d18b15ec9c
Peter-Fagan <peter_fagan@hotmail.com>
2017-06-07 17:08:45 +1000

    Add daily totals to project and refactor

--- lib/timerobot/web/views/project_view.ex
+++ lib/timerobot/web/views/project_view.ex
@@ -17,4 +17,16 @@ defmodule Timerobot.Web.ProjectView do
     times
     |> Enum.reduce(0, fn({_date, _entry, hours}, sum) -> sum + hours end)
   end
+
+  def daily_hours(beginning_of_week, times) do
+    0..6
+    |> Enum.map(fn(day_offset) ->
+      day = Timex.shift(beginning_of_week, days: day_offset)
+      hours =
+        times
+        |> Enum.filter(fn({date, _entry, _hours}) -> date == day end)
+        |> Enum.reduce(0, fn({_date, _entry, hours}, sum) -> sum + hours end)
+      {day, hours}
+    end)
+  end
 end
