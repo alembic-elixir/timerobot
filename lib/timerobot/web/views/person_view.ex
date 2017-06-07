40268c4543aa770d72f70444ce9a1b8dae96d8e7
Peter-Fagan <peter_fagan@hotmail.com>
2017-06-07 16:56:38 +1000

    Add daily totals to person and refactored

--- lib/timerobot/web/views/person_view.ex
+++ lib/timerobot/web/views/person_view.ex
@@ -1,20 +1,20 @@
 defmodule Timerobot.Web.PersonView do
   use Timerobot.Web, :view
 
-  def project_dailies(beginning_of_week, project_times) do
+  def calculate_totals(times) do
+    times
+    |> Enum.reduce(0, fn({_date, _entry, hours}, sum) -> sum + hours end)
+  end
+
+  def daily_hours(beginning_of_week, times) do
     0..6
     |> Enum.map(fn(day_offset) ->
       day = Timex.shift(beginning_of_week, days: day_offset)
       hours =
-        project_times
-        |> Enum.filter(&(elem(&1, 0) == day))
-        |> Enum.reduce(0, &(elem(&1, 2) + &2))
+        times
+        |> Enum.filter(fn({date, _entry, _hours}) -> date == day end)
+        |> Enum.reduce(0, fn({_date, _entry, hours}, sum) -> sum + hours end)
       {day, hours}
     end)
-  end
-
-  def calculate_totals(times) do
-    times
-    |> Enum.reduce(0, fn({_date, _entry, hours}, sum) -> sum + hours end)
   end
 end
