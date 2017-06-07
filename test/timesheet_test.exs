37d905745331da05ab503cbf1698ec103a83121d
Peter-Fagan <peter_fagan@hotmail.com>
2017-06-07 14:58:55 +1000

    Add tests for entry sorting functions

--- test/timesheet_test.exs
+++ test/timesheet_test.exs
@@ -3,6 +3,7 @@ defmodule Timerobot.TimesheetTest do
 
   alias Timerobot.Timesheet
   alias Timerobot.Timesheet.Client
+  alias Timerobot.Timesheet.Entry
 
   @create_attrs %{"name" => "some name"}
   @update_attrs %{"name" => "some updated name", "slug" => "some-updated-name"}
@@ -56,5 +57,63 @@ defmodule Timerobot.TimesheetTest do
   test "change_client/1 returns a client changeset" do
     client = fixture(:client)
     assert %Ecto.Changeset{} = Timesheet.change_client(client)
+  end
+
+  test "test entries_for_person" do
+    assert Timesheet.entries_for_person([]) == []
+    assert Timesheet.entries_for_person([
+      %Entry{person: "person1", date: ~D[2000-01-01], project: "test", hours: 8}
+    ]) == [{~D[1999-12-27], [{~D[2000-01-01], "test", 8}]}]
+    assert Timesheet.entries_for_person([
+      %Entry{person: "person1", date: ~D[2000-01-01], project: "test", hours: 8},
+      %Entry{person: "person2", date: ~D[2000-01-01], project: "test", hours: 8},
+    ]) == [{~D[1999-12-27], [{~D[2000-01-01], "test", 16}]}]
+    assert Timesheet.entries_for_person([
+      %Entry{person: "person1", date: ~D[2000-01-01], project: "test", hours: 8},
+      %Entry{person: "person2", date: ~D[2000-01-01], project: "test", hours: 8},
+      %Entry{person: "person1", date: ~D[2000-01-02], project: "test", hours: 2},
+      %Entry{person: "person2", date: ~D[2000-01-02], project: "test", hours: 2},
+    ]) == [
+      {~D[1999-12-27], [
+        {~D[2000-01-01], "test", 16},
+        {~D[2000-01-02], "test", 4}
+      ]}
+    ]
+    assert Timesheet.entries_for_person([
+      %Entry{person: "person1", date: ~D[2000-01-01], project: "project1", hours: 8},
+      %Entry{person: "person2", date: ~D[2000-01-01], project: "project2", hours: 8},
+    ]) == [{~D[1999-12-27], [
+      {~D[2000-01-01], "project2", 8},
+      {~D[2000-01-01], "project1", 8}
+    ]}]
+  end
+
+  test "test entries_for_project" do
+    assert Timesheet.entries_for_project([]) == []
+    assert Timesheet.entries_for_project([
+      %Entry{person: "person1", date: ~D[2000-01-01], project: "project1", hours: 8}
+    ]) == [{~D[1999-12-27], [{~D[2000-01-01], "person1", 8}]}]
+    assert Timesheet.entries_for_project([
+      %Entry{person: "person1", date: ~D[2000-01-01], project: "project1", hours: 8},
+      %Entry{person: "person2", date: ~D[2000-01-01], project: "project2", hours: 8},
+    ]) == [{~D[1999-12-27], [
+      {~D[2000-01-01], "person2", 8},
+      {~D[2000-01-01], "person1", 8}
+    ]}]
+    assert Timesheet.entries_for_project([
+      %Entry{person: "person1", date: ~D[2000-01-01], project: "project1", hours: 8},
+      %Entry{person: "person1", date: ~D[2000-01-01], project: "project2", hours: 8},
+    ]) == [{~D[1999-12-27], [{~D[2000-01-01], "person1", 16}]}]
+    assert Timesheet.entries_for_project([
+      %Entry{person: "person1", date: ~D[2000-01-01], project: "test", hours: 8},
+      %Entry{person: "person1", date: ~D[2000-01-01], project: "test", hours: 8},
+      %Entry{person: "person2", date: ~D[2000-01-02], project: "test", hours: 2},
+      %Entry{person: "person2", date: ~D[2000-01-02], project: "test", hours: 2},
+    ]) == [
+      {~D[1999-12-27], [
+        {~D[2000-01-01], "person1", 16},
+        {~D[2000-01-02], "person2", 4}
+      ]}
+    ]
   end
 end
