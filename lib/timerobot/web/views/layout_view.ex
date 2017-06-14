b02b9b017a9fba2a059dd80e3ad34d3917ae9d66
Peter-Fagan <peter_fagan@hotmail.com>
2017-06-14 11:17:42 +1000

    Extract project details to partial

--- lib/timerobot/web/views/layout_view.ex
+++ lib/timerobot/web/views/layout_view.ex
@@ -1,3 +1,20 @@
 defmodule Timerobot.Web.LayoutView do
   use Timerobot.Web, :view
+
+  def calculate_totals(times) do
+    times
+    |> Enum.reduce(0, fn({_date, _person, hours}, sum) -> sum + hours end)
+  end
+
+  def daily_hours(beginning_of_week, times) do
+    0..6
+    |> Enum.map(fn(day_offset) ->
+      day = Timex.shift(beginning_of_week, days: day_offset)
+      hours =
+        times
+        |> Enum.filter(fn({date, _person, _hours}) -> date == day end)
+        |> Enum.reduce(0, fn({_date, _person, hours}, sum) -> sum + hours end)
+      {day, hours}
+    end)
+  end
 end
--- lib/timerobot/web/views/project_view.ex
+++ lib/timerobot/web/views/project_view.ex
@@ -1,20 +1,3 @@
 defmodule Timerobot.Web.ProjectView do
   use Timerobot.Web, :view
-
-  def calculate_totals(times) do
-    times
-    |> Enum.reduce(0, fn({_date, _person, hours}, sum) -> sum + hours end)
-  end
-
-  def daily_hours(beginning_of_week, times) do
-    0..6
-    |> Enum.map(fn(day_offset) ->
-      day = Timex.shift(beginning_of_week, days: day_offset)
-      hours =
-        times
-        |> Enum.filter(fn({date, _person, _hours}) -> date == day end)
-        |> Enum.reduce(0, fn({_date, _person, hours}, sum) -> sum + hours end)
-      {day, hours}
-    end)
-  end
 end
