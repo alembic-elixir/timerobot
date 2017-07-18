911cf7d68c433cc50a8382f353e203a46241ddf2
Peter-Fagan <peter_fagan@hotmail.com>
2017-07-18 16:44:07 +1000

    Add logged in user as default person for new entries

--- lib/timerobot/web/Auth/current_user.ex
+++ lib/timerobot/web/Auth/current_user.ex
@@ -6,6 +6,7 @@ defmodule Timerobot.CurrentUser do
 
   def call(conn, _opts) do
     current_user = current_resource(conn)
+    IO.inspect current_user
     assign(conn, :current_user, current_user)
   end
 end
