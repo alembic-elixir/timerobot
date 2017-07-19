61df45960d942243caa7e110a9b1c27e23904a4f
Peter-Fagan <peter_fagan@hotmail.com>
2017-07-19 12:09:36 +1000

    Add days to report

--- lib/timerobot/web/views/report_view.ex
+++ lib/timerobot/web/views/report_view.ex
@@ -1,3 +1,10 @@
 defmodule Timerobot.Web.ReportView do
   use Timerobot.Web, :view
+
+  def calculate_days(hours) do
+    hours_in_day = 8
+    days = hours/hours_in_day
+    Float.ceil(days * 4) / 4
+  end
+
 end
